80849c8f203990c07d1b66b5d0665bcd5637166f
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2023-02-23 17:14:14 -0500

    adding env

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hellooooo");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL
+).then(()=> console.log("DB connection succesful.")).catch((err)=> console.log(err));//since its a promise, we want it to confirm to us
+const PORT = 5000;
+
+
+
+app.listen(process.env.PORT || PORT, ()=> {
+    console.log("Backend server is running onn port: " + PORT);
+});
